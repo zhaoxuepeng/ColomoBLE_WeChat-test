da4fc9062e0be63137336a45cf4f0481591ae453
unknown <zxpkinetis>
2016-06-21 19:23:00 +0800

    update .gitigonre

---
+++ tar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ./Projects/ble/common/cc2540.tar 
+tar -cvf ./Projects/ble/common/cc2540.tar ./Projects/ble/common/cc2540
---
+++ untar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#rm -R ./Projects/ble/common/cc2540
+tar -xvf ./Projects/ble/common/cc2540.tar #-C ./Projects/ble/common/
+#rm ./Projects/ble/common/cc2540.tar 
