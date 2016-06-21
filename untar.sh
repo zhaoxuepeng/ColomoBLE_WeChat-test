bce92938895d0839b3b310992b35c82bdedb33b6
unknown <zxpkinetis>
2016-06-21 19:30:05 +0800

    untar

--- untar.sh
+++ untar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#rm -R ./Projects/ble/common/cc2540
+rm -R ./Projects/ble/common/cc2540
 tar -xvf ./Projects/ble/common/cc2540.tar #-C ./Projects/ble/common/
-#rm ./Projects/ble/common/cc2540.tar 
+rm ./Projects/ble/common/cc2540.tar 
